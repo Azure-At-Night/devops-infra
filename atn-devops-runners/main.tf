c741a60118c9f0997c375e3c3110995c5a39fe3c
Filip Vagner <filip.vagner@orbit.cz>
2025-04-17 10:00:38 +0200

    acr diagnostic settings added

--- atn-devops-runners/main.tf
+++ atn-devops-runners/main.tf
@@ -183,94 +183,24 @@ resource "github_actions_runner_group" "gha_runner_group" {
 #endregion Github runnners groups
 
 #region Custom module
-# module "custom_runner" {
-#   source  = "../runners-module"
+module "custom_runner" {
+  source  = "../runners-module"
 
-#   resource_group_name     = module.rg["rg_004"].name
-#   location                = module.primary_location.name
-#   container_registry_name = "acrrnrsmodule1"
-#   container_registry_sku  = "Premium"
-#   use_private_networking = false
-#   # container_registry_lock = {
-#   #     name       = "acrrnrsmodule1"
-#   #     kind = "CanNotDelete"
-#   # }
-# }
+  resource_group_name     = module.rg["rg_004"].name
+  location                = module.primary_location.name
+  container_registry_name = "acrrnrsmodule1"
+  container_registry_sku  = "Premium"
+  use_private_networking = false
+  # container_registry_lock = {
+  #     name       = "acrrnrsmodule1"
+  #     kind = "CanNotDelete"
+  # }
+  # diagnostic_settings = {
+  #   acr-all = {
+  #     workspace_resource_id = "/subscriptions/018805fe-880b-417d-bf6b-6eccfbefac5a/resourceGroups/rg-manualresources/providers/Microsoft.OperationalInsights/workspaces/log-atn-devops-rnrs"
+  #   }
+  # }
+  
+}
 #endregion Custom module
 
-#region Submodule
-locals {
-  sub_acr_name = "acrsubmodule1"
-}
-module "sub_acr" {
-  source                        = "Azure/avm-res-containerregistry-registry/azurerm"
-  version                       = "0.4.0"
-  name                          = local.sub_acr_name
-  resource_group_name           = module.rg["rg_005"].name
-  location                      = module.primary_location.name
-  public_network_access_enabled = false
-  zone_redundancy_enabled       = var.use_private_networking
-  network_rule_bypass_option    = var.use_private_networking ? "AzureServices" : "None"
-  enable_telemetry              = false
-  # private_endpoints = var.use_private_networking ? {
-  #   container_registry = {
-  #     private_dns_zone_resource_ids = var.private_dns_zone_id == null || var.private_dns_zone_id == "" ? [] : [var.private_dns_zone_id]
-  #     subnet_resource_id            = var.subnet_id
-  #   }
-  # } : null
-  # tags = var.tags
-}
-
-resource "azurerm_container_registry_task" "this" {
-  for_each = var.images
-
-  container_registry_id = module.container_registry.resource_id
-  name                  = each.value.task_name
-  tags                  = var.tags
-
-  docker_step {
-    context_access_token = each.value.context_access_token
-    context_path         = each.value.context_path
-    dockerfile_path      = each.value.dockerfile_path
-    image_names          = each.value.image_names
-  }
-  identity {
-    type = "SystemAssigned" # Note this has to be a System Assigned Identity to work with private networking and `network_rule_bypass_option` set to `AzureServices`
-  }
-  platform {
-    os = "Linux"
-  }
-  registry_credential {
-    custom {
-      login_server = module.container_registry.resource.login_server
-      identity     = "[system]"
-    }
-  }
-}
-
-resource "azurerm_container_registry_task_schedule_run_now" "this" {
-  for_each = var.images
-
-  container_registry_task_id = azurerm_container_registry_task.this[each.key].id
-
-  depends_on = [azurerm_role_assignment.container_registry_push_for_task]
-
-  lifecycle {
-    replace_triggered_by = [azurerm_container_registry_task.this]
-  }
-}
-
-resource "azurerm_role_assignment" "container_registry_pull_for_container_instance" {
-  principal_id         = var.container_compute_identity_principal_id
-  scope                = module.container_registry.resource_id
-  role_definition_name = "AcrPull"
-}
-
-resource "azurerm_role_assignment" "container_registry_push_for_task" {
-  for_each = var.images
-
-  principal_id         = azurerm_container_registry_task.this[each.key].identity[0].principal_id
-  scope                = module.container_registry.resource_id
-  role_definition_name = "AcrPush"
-}
-#endregion Submodule
